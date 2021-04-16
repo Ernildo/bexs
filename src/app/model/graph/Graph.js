14a61245723da39561bc063f31035de30eeb9b3c
Ernildo <ernildopnf@gmail.com>
2021-04-16 12:55:12 -0300

    Modulo Grafo modificado

--- src/app/model/graph/Graph.js
+++ src/app/model/graph/Graph.js
@@ -77,17 +77,4 @@ function Graph(listNodes = []) {
    };
 }
 
-const graph = Graph(['GRU', 'BRC', 'SCL', 'CDG', 'ORL']);
-
-graph.insertConnection('GRU','BRC',10);
-graph.insertConnection('BRC','SCL',5);
-graph.insertConnection('GRU','CDG',75);
-graph.insertConnection('GRU','SCL',20);
-graph.insertConnection('GRU','ORL',56);
-graph.insertConnection('ORL','CDG',5);
-graph.insertConnection('SCL','ORL',20);
-
-console.log(graph.bestRoute('GRU', 'CDG'));
-
-
 module.exports = Graph;
